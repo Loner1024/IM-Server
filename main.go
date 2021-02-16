04040df1addf78c6fa4f7d524107aa43bd33aeba
Loner1024 <xth12138@gmail.com>
2021-02-16 23:55:24 +0800

    user go online && boardcast

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 
 func main(){
 	server:=NewServer("127.0.0.1",8888)
-	Start(server)
+	server.Start()
 }
 
--- server.go
+++ server.go
@@ -4,38 +4,70 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	IP string
 	Port int
+	OnlineMap map[string]*User
+	mapLock sync.RWMutex
+	
+	Message chan string
 }
 
 func NewServer(ip string,port int) *Server{
 	return &Server{
-		IP:   ip,
-		Port: port,
+		IP:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
+}
+
+func (this *Server) ListenMessage(){
+	for{
+		msg:=<-this.Message
+		this.mapLock.Lock()
+		for _,cli:=range this.OnlineMap{
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
 	}
 }
 
 
-func Handler(conn net.Conn){
-	log.Println("连接建立")
+func (this *Server) BoardCast(user *User,msg string){
+	sendMsg:="["+user.Addr+"]"+user.Name+":"+msg
+	this.Message<-sendMsg
 }
 
-func Start(host *Server){
-	listener,err:=net.Listen("tcp",fmt.Sprintf("%s:%d",host.IP,host.Port))
+
+func (this *Server) Handler(conn net.Conn){
+	user:=NewUser(conn)
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+	this.BoardCast(user,"已上线")
+	
+}
+
+func (this *Server) Start(){
+	listener,err:=net.Listen("tcp",fmt.Sprintf("%s:%d",this.IP,this.Port))
 	if err!=nil{
 		log.Println("net.Listen err:",err)
 		return
 	}
 	defer listener.Close()
+	
+	go this.ListenMessage()
+	
 	for {
 		conn,err:=listener.Accept()
 		if err!=nil{
 			log.Println("Listener accept err:",err)
 			continue
 		}
-		go Handler(conn)
+		go this.Handler(conn)
 	}
 }
