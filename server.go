da4b9987713ca9978074a9e1f968dd6f292103b3
Loner1024 <xth12138@gmail.com>
2021-02-19 20:14:54 +0800

    v0.6 user rename

--- server.go
+++ server.go
@@ -9,15 +9,15 @@ import (
 )
 
 type Server struct {
-	IP string
-	Port int
+	IP        string
+	Port      int
 	OnlineMap map[string]*User
-	mapLock sync.RWMutex
-	
+	mapLock   sync.RWMutex
+
 	Message chan string
 }
 
-func NewServer(ip string,port int) *Server{
+func NewServer(ip string, port int) *Server {
 	return &Server{
 		IP:        ip,
 		Port:      port,
@@ -26,75 +26,72 @@ func NewServer(ip string,port int) *Server{
 	}
 }
 
-func (this *Server) ListenMessage(){
-	for{
-		msg:=<-this.Message
+func (this *Server) ListenMessage() {
+	for {
+		msg := <-this.Message
 		this.mapLock.Lock()
-		for _,cli:=range this.OnlineMap{
+		for _, cli := range this.OnlineMap {
 			cli.C <- msg
 		}
 		this.mapLock.Unlock()
 	}
 }
 
-
-func (this *Server) BoardCast(user *User,msg string){
-	sendMsg:="["+user.Addr+"]"+user.Name+":"+msg
-	this.Message<-sendMsg
+func (this *Server) BoardCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
 }
 
+func (this *Server) Handler(conn net.Conn) {
+	user := NewUser(conn, this)
 
-func (this *Server) Handler(conn net.Conn){
-	user:=NewUser(conn,this)
-	
 	// 用户上线 将用户加入 OnlineMap
 	user.Online()
-	
+
 	// 接收客户端发送的消息
 	go func() {
-		buf:=make([]byte,4096)
-		for  {
-			n,err:=conn.Read(buf)
-			if n==0{
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
 				user.OffLine()
 				return
 			}
-			if err!=nil && err!= io.EOF{
-				fmt.Println("Conn Read err:",err)
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err:", err)
 				return
 			}
 			// 提取用户信息
-			msg:=string(buf[:n-1])
+			msg := string(buf[:n-1])
 			// 广播消息
 			user.DoMessage(msg)
 		}
 	}()
-	
+
 	// 阻塞当前 Handler
 	select {}
-	
+
 }
 
-
 // 启动服务器的接口
-func (this *Server) Start(){
+func (this *Server) Start() {
 	// socket listen
-	listener,err:=net.Listen("tcp",fmt.Sprintf("%s:%d",this.IP,this.Port))
-	if err!=nil{
-		log.Println("net.Listen err:",err)
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.IP, this.Port))
+	if err != nil {
+		log.Println("net.Listen err:", err)
 		return
 	}
 	// close listen socket
 	defer listener.Close()
-	
+
 	// 启动监听 Message 的 goroutine
 	go this.ListenMessage()
-	
+
 	for {
 		// accept
-		conn,err:=listener.Accept()
-		if err!=nil{
-			log.Println("Listener accept err:",err)
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Listener accept err:", err)
 			continue
 		}
 		go this.Handler(conn)
--- user.go
+++ user.go
@@ -1,53 +1,80 @@
 package main
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 type User struct {
-	Name string
-	Addr string
-	C chan string
-	conn net.Conn
+	Name   string
+	Addr   string
+	C      chan string
+	conn   net.Conn
 	server *Server
 }
 
-func NewUser(conn net.Conn,server *Server) *User{
-	user:=&User{
-		Name: conn.RemoteAddr().String(),
-		Addr: conn.RemoteAddr().String(),
-		C:    make(chan string),
-		conn: conn,
+func NewUser(conn net.Conn, server *Server) *User {
+	user := &User{
+		Name:   conn.RemoteAddr().String(),
+		Addr:   conn.RemoteAddr().String(),
+		C:      make(chan string),
+		conn:   conn,
 		server: server,
 	}
 	go user.ListenMessage()
 	return user
 }
 
-func (this *User) Online(){
+func (this *User) Online() {
 	// 用户上线 将用户加入 OnlineMap
 	this.server.mapLock.Lock()
 	this.server.OnlineMap[this.Name] = this
 	this.server.mapLock.Unlock()
 	// 广播用户上线信息
-	this.server.BoardCast(this,"已上线")
+	this.server.BoardCast(this, "已上线")
 }
 
-func (this *User) OffLine(){
+func (this *User) OffLine() {
 	// 用户下线 将用户从 OnlineMap 删除
 	this.server.mapLock.Lock()
-	delete(this.server.OnlineMap,this.Name)
+	delete(this.server.OnlineMap, this.Name)
 	this.server.mapLock.Unlock()
 	// 广播用户下线信息
-	this.server.BoardCast(this,"下线")
+	this.server.BoardCast(this, "下线")
+}
+
+// 给当前用户发送消息（自己给自己发消息
+func (this *User) SendMessage(msg string) {
+	this.conn.Write([]byte(msg))
 }
 
 // 用户消息处理
-func (this *User) DoMessage(msg string){
-	this.server.BoardCast(this,msg)
+func (this *User) DoMessage(msg string) {
+	if msg == "who" {
+		for _, user := range this.server.OnlineMap {
+			this.SendMessage("[" + user.Addr + "]" + user.Name + "在线...\n")
+		}
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		newName := strings.Split(msg, "|")[1]
+		if _, ok := this.server.OnlineMap[newName]; !ok {
+			// 修改 name
+			this.server.mapLock.Lock()
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[newName] = this
+			this.server.mapLock.Unlock()
+			this.Name = newName
+			this.SendMessage("名字已修改为: " + newName + "\n")
+		} else {
+			this.SendMessage("名字已经存在\n")
+		}
+	} else {
+		this.server.BoardCast(this, msg)
+	}
 }
 
-func (this *User) ListenMessage(){
-	for  {
-		msg:=<-this.C
-		this.conn.Write([]byte(msg+"\n"))
+func (this *User) ListenMessage() {
+	for {
+		msg := <-this.C
+		this.conn.Write([]byte(msg + "\n"))
 	}
 }
